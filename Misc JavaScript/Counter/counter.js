aa441883cb44521ca1d0aa6ecc0c8291a3b6e872
dtlancaster <Tuscarora007@icloud.com>
2021-03-28 19:01:18 -0400

    upload Counter

---
+++ Misc JavaScript/Counter/counter.js
@@ -0,0 +1,39 @@
+// set initial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector('#value');
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(function(item) 
+{
+    item.addEventListener('click', function(e)
+    {
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease'))
+        {
+            count--;
+        }
+        else if (styles.contains('increase'))
+        {
+            count++;
+        }
+        else 
+        {
+            count = 0;
+        }
+        if (count > 0)
+        {
+            value.style.color = 'green';
+        }
+        if (count < 0)
+        {
+            value.style.color = 'red';
+        }
+        if (count == 0)
+        {
+            value.style.color = '#222'
+        }
+        value.textContent = count;
+    });
+});
