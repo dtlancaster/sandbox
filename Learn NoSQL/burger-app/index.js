b72dc277af0cb15854b33f3cef16e612e027876b
dtlancaster <Tuscarora007@icloud.com>
2021-12-10 00:07:20 -0500

    project setup

---
+++ Learn NoSQL/burger-app/index.js
@@ -0,0 +1,13 @@
+const PORT = 8000;
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const fetch = require("node-fetch");
+require("dotenv").config();
+morgan("tiny");
+
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
